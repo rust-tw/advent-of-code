205fa358348efe92c46f0de20243d886f47c7fc8
pan93412 <pan93412@gmail.com>
2022-12-01 13:37:53 +0800

    test: Refactor the test entries

--- 2022/01/pan93412/tests/test_aoc.rs
+++ 2022/01/pan93412/tests/test_aoc.rs
@@ -1,14 +1,24 @@
 #[cfg(test)]
 mod tests {
-    use pan93412_aoc_2022::AocInput;
+    use pan93412_aoc_2022::{AocInput, AocInputBlock};
 
-    #[test]
-    fn test_aoc() {
+    fn get_input_block() -> AocInputBlock {
         let i = include_str!("./testdata.txt");
         let input = AocInput::new(i);
-        let blk = input.get_sum_block();
+        input.get_sum_block()
+    }
+
+    #[test]
+    fn test_aoc_answer_one() {
+        let blk = self::get_input_block();
 
         assert_eq!(blk.get_answer_one(), 24000);
+    }
+
+    #[test]
+    fn test_aoc_answer_two() {
+        let blk = self::get_input_block();
+
         assert_eq!(blk.get_answer_two(), 45000);
     }
 }
