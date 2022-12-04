303e46b7d905785ea2ce9a93f58da8890ff8cfeb
Kenneth Lo <closer.tw@gmail.com>
2022-12-04 22:52:38 +0800

    Day 4 Part 2

--- 2022/04/i-am-closer/src/lib.rs
+++ 2022/04/i-am-closer/src/lib.rs
@@ -1,20 +1,29 @@
 use std::error::Error;
 use std::str::FromStr;
 
-pub fn challenge_04(lines: &Vec<&str>) -> u64 {
-    lines
-        .iter()
-        .map(|&line| {
-            let v = line
-                .split(',')
+pub fn challenge_04(lines: Vec<&str>) -> (u64, u64) {
+    let v = lines
+        .into_iter()
+        .map(|line| {
+            line.split(',')
                 .map(|s| s.parse::<Section>().unwrap())
-                .collect::<Vec<_>>();
-            let s0 = v.get(0).unwrap();
-            let s1 = v.get(1).unwrap();
-            s0.contains(s1) || s1.contains(s0)
+                .collect::<Vec<_>>()
         })
-        .filter(|contained| *contained)
-        .count() as u64
+        .collect::<Vec<_>>();
+
+    let part1 = v
+        .iter()
+        .map(|sections| sections[0].contains(&sections[1]) || sections[1].contains(&sections[0]))
+        .filter(|x| *x)
+        .count() as u64;
+
+    let part2 = v
+        .iter()
+        .map(|sections| sections[0].overlaps(&sections[1]))
+        .filter(|x| *x)
+        .count() as u64;
+
+    (part1, part2)
 }
 
 #[derive(Debug, PartialEq)]
@@ -27,6 +36,16 @@ impl Section {
     fn contains(&self, other: &Self) -> bool {
         self.begin <= other.begin && self.end >= other.end
     }
+
+    fn overlaps(&self, other: &Self) -> bool {
+        if self.begin < other.begin {
+            self.end >= other.begin
+        } else if self.begin > other.begin {
+            other.end >= self.begin
+        } else {
+            true
+        }
+    }
 }
 
 #[derive(Debug, PartialEq)]
@@ -35,6 +54,8 @@ enum ParseSectionError {
     InvalidValues,
 }
 
+// 利用這個 trait，將所有有作 Error trait 的 Error type 都能轉成 ParseSectionError。
+// 這樣下面在使用時，就可以用 `?` operator 將任意的 Error type 轉成 ParseSectionError。
 impl<T> From<T> for ParseSectionError
 where
     T: Error,
@@ -87,4 +108,21 @@ fn test_if_contains() {
     assert!(!test("2-4", "6-8"));
     assert!(test("2-8", "3-7"));
     assert!(test("4-6", "6-6"));
+}
+
+#[test]
+fn test_if_overlaps() {
+    let test = |s1: &str, s2: &str| {
+        s1.parse::<Section>()
+            .unwrap()
+            .overlaps(&(s2.parse::<Section>().unwrap()))
+    };
+
+    assert!(!test("2-4", "6-8"));
+    assert!(!test("4-5", "2-3"));
+    // TODO: 應該要有更全面的 test cases。
+    assert!(test("1-10", "4-7"), "left contains right");
+    assert!(test("2-9", "1-10"), "right contains left");
+    assert!(test("1-7", "3-10"), "left tail overlaps right head");
+    assert!(test("3-10", "1-7"), "left head overlaps right tail");
 }
--- 2022/04/i-am-closer/src/main.rs
+++ 2022/04/i-am-closer/src/main.rs
@@ -5,7 +5,7 @@ fn main() -> io::Result<()> {
     let input = io::read_to_string(io::stdin())?;
     let lines: Vec<_> = input.lines().collect();
 
-    let answers = my_lib::challenge_04(&lines);
+    let answers = my_lib::challenge_04(lines);
 
     println!("{:?}", answers);
 
