8e910e795e4250da2b3cbf07cebca49e6bd58b21
hms5232 <hms5232@hhming.moe>
2024-12-06 19:47:43 +0800

    hms5232 2024 day1 part1 refactor
    誤會題目意思了，所以修改一下寫法
    移除不必要的邏輯並且簡化寫法

--- 2024/01/hms5232/src/main.rs
+++ 2024/01/hms5232/src/main.rs
@@ -6,64 +6,26 @@ fn main() {
     let mut left_list: Vec<u32> = Vec::new();
     let mut right_list: Vec<u32> = Vec::new();
 
+    // read and parse each line then put them into vector
     for line in contents.lines() {
-        let record = line.split("   ").collect::<Vec<_>>();
-        left_list.push(record.get(0).unwrap().parse().unwrap());
-        right_list.push(record.get(1).unwrap().parse().unwrap());
+        let (left, right) = line
+            .split_once("   ")
+            .map(|(l, r)| (l.parse().unwrap(), r.parse().unwrap()))
+            .unwrap();
+        left_list.push(left);
+        right_list.push(right);
     }
 
-    // 取得排序後的 index
-    let left_sorted = argsort(&left_list);
-    let right_sorted = argsort(&right_list);
+    // sort
+    left_list.sort();
+    right_list.sort();
 
-    let mut result: u32 = 0;
-
-    for i in 0..left_sorted.len() {
-        result += left_list[left_sorted[i]].abs_diff(right_list[right_sorted[i]]);
-    }
+    // calculate the sum of the absolute differences
+    let result: u32 = left_list
+        .iter()
+        .zip(&right_list)
+        .map(|(l, r)| l.abs_diff(*r))
+        .sum();
 
     print!("part1: {}", result);
-}
-
-/// Get the indices that sort a vector.
-///
-/// This is from https://stackoverflow.com/a/69764256
-///
-/// ## Example
-///
-/// ```rust
-/// let v = vec![1, 7, 4, 2];
-/// let i = argsort(&v);
-/// assert_eq!(i, &[0, 3, 2, 1]);
-/// ```
-fn argsort<T: Ord>(data: &[T]) -> Vec<usize> {
-    let mut indices = (0..data.len()).collect::<Vec<_>>();
-    indices.sort_by_key(|&i| &data[i]);
-    indices
-}
-
-#[cfg(test)]
-mod test {
-    use super::argsort;
-
-    #[test]
-    fn test_argsort() {
-        let v = vec![1, 7, 4, 2];
-        let i = argsort(&v);
-        assert_eq!(i, &[0, 3, 2, 1]);
-    }
-
-    #[test]
-    fn test_argsort_with_example_left() {
-        let v = vec![3, 4, 2, 1, 3, 3];
-        let i = argsort(&v);
-        assert_eq!(i, &[3, 2, 0, 4, 5, 1]);
-    }
-
-    #[test]
-    fn test_argsort_with_example_right() {
-        let v = vec![4, 3, 5, 3, 9, 3];
-        let i = argsort(&v);
-        assert_eq!(i, &[1, 3, 5, 0, 2, 4]);
-    }
 }
