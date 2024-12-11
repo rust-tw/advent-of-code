0e9a2e04a571b67db5d7f1081b754ebe4a35ebd8
hms5232 <hms5232@hhming.moe>
2024-12-11 18:41:20 +0800

    hms5232 2024 day3 part2

--- 2024/03/hms5232/src/main.rs
+++ 2024/03/hms5232/src/main.rs
@@ -5,6 +5,8 @@ fn main() {
     let file_path = "input.txt"; // change this to the path of your file
     let contents = std::fs::read_to_string(file_path).expect("File Read error");
 
+    /* Part 1 */
+
     let re = Regex::new(r"mul\(\d{1,3},\d{1,3}\)").unwrap();
     let matches: Vec<_> = re.find_iter(&contents).map(|m| {
         m.as_str().replace("mul(", "").replace(")", "")
@@ -16,4 +18,37 @@ fn main() {
     }).sum::<u32>();
 
     println!("Part 1: {}", part1_answer);
+
+    /* Part 2 */
+
+    let re = Regex::new(r"mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)").unwrap();
+    let matches: Vec<_> = re.find_iter(&contents).map(|m| m.as_str()).collect();
+    let mut control_flag = true;
+    let part2_answer: u32 = matches
+        .iter()
+        .map(|m| match *m {
+            "do()" => {
+                control_flag = true;
+                return 0;
+            }
+            "don't()" => {
+                control_flag = false;
+                return 0;
+            }
+            _ => {
+                if !control_flag {
+                    return 0;
+                }
+                let nums: Vec<u32> = m
+                    .replace("mul(", "")
+                    .replace(")", "")
+                    .split(",")
+                    .map(|n| n.parse().unwrap())
+                    .collect();
+                nums[0] * nums[1]
+            }
+        })
+        .sum();
+
+    println!("Part 2: {}", part2_answer);
 }
