50a1c45c834972e146dea1ff9772fb0b8411d97a
hms5232 <hms5232@hhming.moe>
2024-12-11 16:36:43 +0800

    hms5232 2024 day2 part2

--- 2024/02/hms5232/src/main.rs
+++ 2024/02/hms5232/src/main.rs
@@ -9,6 +9,8 @@ fn main() {
         x.split(" ").map(|x| x.parse().unwrap()).collect::<Vec<u32>>()
     }).collect::<Vec<Vec<u32>>>();
 
+    /* Part 1 */
+
     let part1_answer = reports.iter().map(|report| {
         // If "safe", both of these should be true:
         // 1. The levels are either all increasing or all decreasing.
@@ -16,7 +18,7 @@ fn main() {
 
         // sorted => increasing
         // sorted after reverse => decreasing
-        if report.is_sorted() || report.iter().rev().collect::<Vec<_>>().is_sorted() {
+        if is_sorted(report) {
             let mut prev_level = report[0];
             return report.iter().skip(1).all(|level| {
                 // check level and next level differ by at least one and at most three
@@ -31,4 +33,49 @@ fn main() {
     }).filter(|check| *check).count();
 
     println!("Part 1: {}", part1_answer);
+
+    /* Part 2 */
+
+    let part2_answer = reports.iter().filter(|report| {
+        // If "safe", both of these should be true:
+        // 1. The levels are either all increasing or all decreasing.
+        // 2. Any two adjacent levels differ by at least one and at most three.
+
+        let mut is_safe = false;
+        'outer: for i in 0..report.len() {
+            let mut clone_report = (*report).to_owned();
+            clone_report.remove(i);
+
+            // Step 1: check order first
+            if !is_sorted(&clone_report) {
+                continue;
+            }
+
+            // Step 2: check the distance between each level
+            let mut prev_level = clone_report[0];
+            for j in clone_report.iter().skip(1) {
+                if j.abs_diff(prev_level) > 3 || j.eq(&prev_level) {
+                    // If any two adjacent levels differ by more than three or equal, the report is not safe.
+                    // We can jump to next round
+                    continue 'outer;
+                }
+                prev_level = *j;
+            }
+
+            // pass all tests
+            is_safe = true;
+            break;
+        }
+        is_safe
+    }).count();
+
+    println!("Part 2: {}", part2_answer);
+}
+
+/// Determine whether the report is sorted (increasing or decreasing)
+///
+/// * sorted => increasing
+/// * sorted after reverse => decreasing
+fn is_sorted(report: &Vec<u32>) -> bool {
+    report.is_sorted() || report.iter().rev().collect::<Vec<_>>().is_sorted()
 }
